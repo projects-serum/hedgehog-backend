244bd3fd3833b4b7cc4af89eb1bf537158886da1
projects-serum <networks.akash@gmail.com>
2024-03-06 17:47:45 +0700

    w

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const ethers = require("ethers")
 const abi = require("./abi.json")
 const provider = new ethers.JsonRpcProvider("https://base-sepolia-rpc.publicnode.com")
-const wallet = new ethers.Wallet("0xc57dab39187abaec90f5a470f208cb4fdbd952544f2a040353f111a296ff87aa").connect(provider)
+const wallet = new ethers.Wallet(process.env.PK).connect(provider)
 const contract = new ethers.Contract("0x2f847Dbf9f0F14b98b9053bFd49B52d4e1f84cD3", abi, wallet)
+require('dotenv').config();
 
 async function main() {
     const date = Math.floor(Date.now() / 1000)
@@ -15,6 +16,7 @@ async function main() {
     }
 
 }
+
 
 provider.on("block", async (blk) => {
     const end = await main()
